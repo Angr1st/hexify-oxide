09f33aaee0f7349499b751ec8982cb4c1ffa8473
Torben Clasen <t.clasen@outlook.com>
2023-11-19 22:32:41 +0100

    Add path name

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{extract::Path, response::IntoResponse, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn hello_name(Path(name): Path<String>) -> impl IntoResponse {
+    format!("Hello, {}!", name)
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/:name", get(hello_name));
 
     Ok(router.into())
 }
